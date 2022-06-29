620e143a2d079a36de4f00c3f97a9b70373eb8d4
Stanik <stanik@tuta.io>
2022-06-29 20:29:25 +0200

    A working container with a web interface for a supervisor and logs from the python script.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.10.5-slim-buster
 
+EXPOSE 9001
+
 RUN apt-get -y update && apt-get -y upgrade
 RUN apt-get -y install supervisor
 
--- script.py
+++ script.py
@@ -1,4 +1,35 @@
-from time import sleep
+import logging
+import os
+import sys
+import time
 
-while True:
-    sleep(10)
+# INIT LOGGING
+script_name = sys.argv[0][sys.argv[0].rfind('/') + 1:-3]
+logs_dir = 'log/'
+logs_path = f'{logs_dir}/{script_name}.log'
+log_format = '%(asctime)s @%(name)s [%(levelname)s]:    %(message)s'
+
+try:
+    open(logs_path)
+except IOError:
+    os.makedirs(os.path.dirname(logs_dir), exist_ok=True)
+    file = open(logs_path, 'x')
+    file.close()
+
+logging.basicConfig(filename=logs_path,
+                    filemode="a",
+                    format=log_format,
+                    level=logging.INFO)
+
+logger = logging.getLogger()
+
+logger.info('Start loging')
+
+if __name__ == '__main__':
+    try:
+        logger.info('Custom service started')
+        while True:
+            # here for the sake of example, actually doing real stuff here
+            time.sleep(2)
+    finally:
+        logger.info('Custom service stopped')
